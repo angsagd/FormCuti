fccdb7199f363d48e06d616ad33e2809678d82cd
angsagd <2133242+angsagd@users.noreply.github.com>
2024-07-01 10:44:39 +0800

    feat: Add manifest file and service worker for PWA functionality

---
+++ js/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"21b95a01770d61d756d482e41e63aad5","url":"form-init.js"},{"revision":"82770578cc9b4d52cc1008e8ea18cdb5","url":"pdf2htmlEX.js"}]);
